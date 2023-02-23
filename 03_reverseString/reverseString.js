c3f6f0aa4cb058326c286edcc5302fc91efb2967
bakeungdickson@gmail.com <pierodics@gmail.com>
2023-02-24 00:52:02 +0100

    solved exercise 03 reverse a single word

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
+function reverseString(str) {
+    let reversedStr = "";
+    for (let i = str.length - 1; i >= 0; i--) {
+      reversedStr += str[i];
+    }
+    return reversedStr;
+  }
+  
+  let str = "hello";
 
-};
-
+  let reversedStr = reverseString(str);
 // Do not edit below this line
 module.exports = reverseString;
