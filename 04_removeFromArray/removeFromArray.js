47af3cd1f6765f6839e2e84e1be68e5ccaaa6f1c
bakeungdickson@gmail.com <pierodics@gmail.com>
2023-02-25 22:48:04 +0100

    solved exercise 04 remove a single value

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,11 @@
-const removeFromArray = function() {
-
-};
+function removeFromArray(arr, element) {
+    const index = arr.indexOf(element);
+    if (index !== -1) {
+      arr.splice(index, 1);
+    }
+    return arr;
+  }
+//const removeFromArray = function() {
 
 // Do not edit below this line
 module.exports = removeFromArray;
