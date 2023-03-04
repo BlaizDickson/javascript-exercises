63cab95c494f8d35998afceb274ef44de5e7cd12
bakeungdickson@gmail.com <pierodics@gmail.com>
2023-03-04 11:51:43 +0100

    solutin to exercise 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,36 @@
-const sumAll = function() {
-
-};
-
+function sumAll(start, end) {
+  // Check if inputs are numbers and non-negative
+  if (typeof start !== 'number' || typeof end !== 'number' || start < 0 || end < 0) {
+    return "ERROR";
+  }
+  
+  let sum = 0;
+  let increment = start <= end ? 1 : -1;
+  
+  for (let i = start; i !== end + increment; i += increment) {
+    sum += i;
+  }
+  
+  return sum;
+}
+// const sumAll = function(start, end) {
+//     if (start < 0 || end < 0) {
+//       return "ERROR";
+//     }
+  
+//     let sum = 0;
+//     for (let i = start; i <= end; i++) {
+//       sum += i;
+//     }
+//     return sum;
+//   }
+/*const sumAll = function(start, end) {
+    let sum = 0;
+    for (let i = start; i <= end; i++) {
+      sum += i;
+    }
+    return sum;
+  }
+*/
 // Do not edit below this line
 module.exports = sumAll;
